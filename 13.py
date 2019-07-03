1a4e293f96336d39bff4522e657034a93a1a7a4d
logeswari-j <52063194+logeswari-j@users.noreply.github.com>
2019-07-03 11:48:42 +0530

    Update 13.py

--- 13.py
+++ 13.py
@@ -2,7 +2,7 @@ num =int(input())
 if (num>1):   
    for i in range(2, num//2):   
        if (num %i) == 0: 
-           print("no") 
+           print("no")
            break
    else: 
        print("yes") 
