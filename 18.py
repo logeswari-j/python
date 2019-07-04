cd827bb6e782b4bc09616a057b59595f8dc82746
logeswari-j <52063194+logeswari-j@users.noreply.github.com>
2019-07-04 10:55:14 +0530

    Update 18.py

--- 18.py
+++ 18.py
@@ -1,10 +1,10 @@
-n1,n2=map(int,input().split())
-for n in range(n1+1,n2):
-  sum=0
-  temp0=n
-  while temp0>0:
-    digit=temp0%10
-    sum+=digit**3
-    temp0//=10
-  if n==sum:
-    print(n,end=' ')
+g1,g2=map(int,input().split())
+for g in range(g1+1,g2):
+ sum=0
+ temp=g
+ while temp>0:
+  digit=temp%10
+  sum+=digit**3
+  temp//=10
+ if g==sum:
+    print(g,end=" ")
