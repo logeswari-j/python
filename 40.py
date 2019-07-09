5c36f5220c03720defad93c6f13d1698b201ea87
logeswari-j <52063194+logeswari-j@users.noreply.github.com>
2019-07-09 14:34:53 +0530

    Update 40.py

--- 40.py
+++ 40.py
@@ -1,10 +1,10 @@
 snum=int(input())
-s1=1
-s2=1
+a1=1
+b2=1
 count=0
 while(count<snum):
-  print(s1, end=' ')
-  s3=s1+s2
-  s1=s2
-  s2=s3
+  print(a1, end=' ')
+  c3=a1+b2
+  a1=b2
+  b2=c3
   count+=1
