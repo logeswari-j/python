f50ce21c5b3e42124638ea2901effb1a691bba30
logeswari-j <52063194+logeswari-j@users.noreply.github.com>
2019-07-12 23:59:01 -0700

    Update 95.py

--- 95.py
+++ 95.py
@@ -1,8 +1,8 @@
-g11,g22=input().split()
-n1=abs(len(g22)-len(g11))
-for i in range(len(g11)):
- if(len(g22)==1 and g22[i] in g11):
-  break
- if(g11[i]!=g22[i]):
-  n1=n1+1
-print(n1)
+ad,sd=input().split()
+ss1=abs(len(sd)-len(ad))
+for i in range(len(ad)):
+  if(len(sd)==1 and sd[i] in ad):
+    break
+  if(ad[i]!=sd[i]):
+    ss1=ss1+1
+print(ss1)
