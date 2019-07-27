45c8435aab1281db32055604a58869dba2bb2576
logeswari-j <52063194+logeswari-j@users.noreply.github.com>
2019-07-27 08:59:54 +0530

    Update b106.py

--- b106.py
+++ b106.py
@@ -1,7 +1,7 @@
-n1,k2=map(int,input().split())
-intg=input().split()
+x1,y2=map(int,input().split())
+lm=input().split()
 a=[]
-for i in intg:
+for i in lm:
   if (int(i)%2!=0):
     a.append(i)
-print(a[k2-1])
+print(a[y2-1])
