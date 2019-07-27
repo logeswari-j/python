e47a32a3a49cdb1c3b49973a3e8989adc762904f
logeswari-j <52063194+logeswari-j@users.noreply.github.com>
2019-07-27 13:34:31 +0530

    Update pl9.py

--- pl9.py
+++ pl9.py
@@ -1,3 +1,10 @@
-mike=int(input())
-car=mike
-print(car)
+mod,moud=map(int,input().split())
+count=0
+for i in range(mod,moud+1):
+    if i>1:
+        for j in range(2,i):
+            if(i%j==0):
+                break
+        else:
+            count=count+1
+print(count)
