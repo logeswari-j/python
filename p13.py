b6a58dd228f9a6dd46aa4d2484fb04d7566c09d0
logeswari-j <52063194+logeswari-j@users.noreply.github.com>
2019-07-27 14:38:10 +0530

    Update p13.py

--- p13.py
+++ p13.py
@@ -1,7 +1,7 @@
 god,great=input().split()
 cost=abs(len(great)-len(god))
 for g in range(len(god)):
-    if(len(great)==1 and great[g] in strg1):
+    if(len(great)==1 and great[g] in god):
         break
     if (god[g]!=great[g]):
         cost=cost+1
